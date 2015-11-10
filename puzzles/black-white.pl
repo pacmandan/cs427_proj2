6725829bb660cdc2520ce04d8a07615e9ef746e9
rosasynstylae <64bitsuperhero@gmail.com>
2015-11-09 18:32:34 -0700

    worked on black-whit.pl

--- puzzles/black-white.pl
+++ puzzles/black-white.pl
@@ -1,3 +1,123 @@
 /**
   * Black-White problem definition
   */
+
+%State definition: [A, B, C, D, E, F, G]
+/* 
+  W: Number of white tiles to the left of Black tiles
+  B: Position of the blank tile
+ */
+
+start([b,b,b,blank,w,w,w]).
+goal([w,w,w,blank,b,b,b]).
+goal([w,blank,w,w,b,b,b]).
+goal([blank,w,w,w,b,b,b]).
+goal([w,w,blank,w,b,b,b]).
+goal([w,w,w,b,blank,b,b]).
+goal([w,w,w,b,b,blank,b]).
+goal([w,w,w,b,b,b,blank]). 
+
+
+/* [blank, A, B, C, D, E, F] */
+
+arc([blank, A, B, C, D, E, F], Child, 1) :-
+    Child is [A, blank, B, C, D, E, F].
+
+arc([blank, A, B, C, D, E, F], Child, 1) :-
+    Child is [B, A, blank, C, D, E, F].
+
+arc([blank, A, B, C, D, E, F], Child, 2) :-
+    Child is [C, A, B, blank, D, E, F].
+
+/* [A, blank, B, C, D, E, F] */
+
+arc([A, blank, B, C, D, E, F], Child, 1) :-
+    Child is [blank, A, B, C, D, E, F].
+
+arc([A, blank, B, C, D, E, F], Child, 1) :-
+    Child is [A, B, blank, C, D, E, F].
+
+arc([A, blank, B, C, D, E, F], Child, 1) :-
+    Child is [A, C, B, blank, D, E, F].
+
+arc([A, blank, B, C, D, E, F], Child, 2) :-
+    Child is [A, D, B, C, blank, E, F].
+
+/* [A, B, blank, C, D, E, F] */
+
+arc([A, B, blank, C, D, E, F], Child, 1) :-
+    Child is [A, blank, B, C, D, E, F].
+
+arc([A, B, blank, C, D, E, F], Child, 1) :-
+    Child is [A, B, C, blank, D, E, F].
+
+arc([A, B, blank, C, D, E, F], Child, 1) :-
+    Child is [blank, B, A, C, D, E, F].
+
+arc([A, B, blank, C, D, E, F], Child, 1) :-
+    Child is [A, B, D, C, blank, E, F].
+
+arc([A, B, blank, C, D, E, F], Child, 1) :-
+    Child is [A, B, E, C, D, blank, F].
+
+/* [A, B, C, blank, D, E, F] */
+
+arc([A, B, C, blank, D, E, F], Child, 1) :-
+    Child is [A, B, blank, C, D, E, F].
+
+arc([A, B, C, blank, D, E, F], Child, 1) :-
+    Child is [A, B, C, D, blank, E, F].
+
+arc([A, B, C, blank, D, E, F], Child, 1) :-
+    Child is [A, blank, C, B, D, E, F].
+
+arc([A, B, C, blank, D, E, F], Child, 1) :-
+    Child is [A, B, C, E, D, blank, F].
+
+arc([A, B, C, blank, D, E, F], Child, 2) :-
+    Child is [blank, B, C, A, D, E, F].
+
+arc([A, B, C, blank, D, E, F], Child, 2) :-
+    Child is [A, B, C, F, D, E, blank].
+
+/* [A, B, C, D, blank, E, F] */
+
+arc([A, B, C, D, blank, E, F], Child, 1) :-
+    Child is [A, B, C, blank, D, E, F].
+
+arc([A, B, C, D, blank, E, F], Child, 1) :-
+    Child is [A, B, C, D, E, blank, F].
+
+arc([A, B, C, D, blank, E, F], Child, 1) :-
+    Child is [A, B, blank, D, C, E, F].
+
+arc([A, B, C, D, blank, E, F], Child, 1) :-
+    Child is [A, B, C, D, F, E, blank].
+
+arc([A, B, C, D, blank, E, F], Child, 2) :-
+    Child is [A, blank, C, D, B, E, F].
+
+/* [A, B, C, D, E, blank, F] */
+
+arc([A, B, C, D, E, blank, F], Child, 1) :-
+    Child is [A, B, C, D, blank, E, F].
+
+arc([A, B, C, D, E, blank, F], Child, 1) :-
+    Child is [A, B, C, D, E, F, blank].
+
+arc([A, B, C, D, E, blank, F], Child, 1) :-
+    Child is [A, B, C, blank, E, D, F].
+
+arc([A, B, C, D, E, blank, F], Child, 2) :-
+    Child is [A, B, blank, D, E, C, F].
+
+/* [A, B, C, D, E, F, blank] */
+
+arc([A, B, C, D, E, F, blank], Child, 1) :-
+    Child is [A, B, C, D, E, blank, F].
+
+arc([A, B, C, D, E, F, blank], Child, 1) :-
+    Child is [A, B, C, D, blank, F, E].
+
+arc([A, B, C, D, E, F, blank], Child, 2) :-
+    Child is [A, B, C, blank, E, F, D].
